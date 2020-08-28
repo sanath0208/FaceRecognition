5147e0347c62ba202e41f1d5523daef191babd75
sanath0208 <53268864+sanath0208@users.noreply.github.com>
2020-08-28 10:19:06 +0530

    added logo and Nav

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'tachyons';
+import 'react-tilt'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
